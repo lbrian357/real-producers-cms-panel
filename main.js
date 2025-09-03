a76596889216ea6207d34f9583383f4bc49688ae
Brian Li <lbrian357@gmail.com>
2025-09-03 15:14:58 -0700

    Change partner search sort order to api

--- main.js
+++ main.js
@@ -1397,7 +1397,7 @@ var rpLib = {
 
     fetchAllPartners: function(brandId) {
       return new Promise((resolve) => {
-        const url = `https://vhpb1dr9je.execute-api.us-east-1.amazonaws.com/dev/https://api.webflow.com/v2/collections/${PARTNERS_COLLECTION_ID}/items/live?sortBy=lastPublished&sortOrder=desc`;
+        const url = `https://vhpb1dr9je.execute-api.us-east-1.amazonaws.com/dev/https://api.webflow.com/v2/collections/${PARTNERS_COLLECTION_ID}/items/live?sortBy=name&sortOrder=asc`;
         
         this.state.allPartners = [];
         
